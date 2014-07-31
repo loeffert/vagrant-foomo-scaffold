aa27c3c8c29d0027051e3b5cf5ac6046f804e8d2
Frederik Löffert <frederik@zitrusmedia.de>
2014-07-31 02:46:22 +0200

    minor provisioning improvements

--- provisioning/shell/00-bootstrap.sh
+++ provisioning/shell/00-bootstrap.sh
@@ -1,5 +1,6 @@
 echo "bootstrap shell provisioning: start"
 
+export PROJECT_NAME='project'
 export FOOMO_PROVISIONING=`true`
 
 echo "bootstrap shell provisioning: end"
--- provisioning/shell/40-mongo.sh
+++ provisioning/shell/40-mongo.sh
@@ -1,5 +1,6 @@
 echo "mongo shell provisioning: start"
 
+#requires setting the mongo apt server during apt provisioning
 apt-get install mongodb-10gen
 
 echo "mongo shell provisioning: end"
